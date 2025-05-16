0b1a8c431e0396ccf5ceb342479a98a024f8c496
Kotlovskiy <YA111EP@mail.ru>
2025-05-16 14:20:08 +0700

    Ужас

---
+++ app/src/main/java/com/unewexp/superblockly/blocks/voidBlocks/PrintBlock.kt
@@ -0,0 +1,35 @@
+package com.unewexp.superblockly.blocks.voidBlocks
+
+import com.example.myfirstapplicatioin.model.Connector
+import com.unewexp.superblockly.blocks.ExecutionContext
+import com.unewexp.superblockly.enums.BlockType
+import com.unewexp.superblockly.enums.ConnectorType
+import java.lang.IllegalStateException
+import java.util.UUID
+
+class PrintBlock(
+    id: UUID = UUID.randomUUID(),
+    blockType: BlockType = BlockType.PRINT_BLOCK
+) : VoidBlock(id, blockType) {
+
+    val inputConnector = Connector(
+        connectionType = ConnectorType.INPUT,
+        sourceBlock = this,
+        allowedBlockTypes = setOf(
+            BlockType.VARIABLE_REFERENCE,
+            BlockType.STRING_CONCAT,
+            BlockType.INT_LITERAL,
+            BlockType.OPERAND,
+            BlockType.BOOLEAN_LITERAL,
+            BlockType.STRING_LITERAL,
+        )
+    )
+
+    override fun execute() {
+        val value = inputConnector.connectedTo?.evaluate()
+            ?: throw IllegalStateException("В PrintBlock не добавлено значение вывода")
+
+        //ExecutionContext.appendLog(value.toString());
+    }
+
+}
---
+++ app/src/main/java/com/unewexp/superblockly/test.kt
@@ -0,0 +1,13 @@
+package com.unewexp.superblockly
+
+import com.example.myfirstapplicatioin.utils.connectTo
+import com.unewexp.superblockly.blocks.StartBlock
+import com.unewexp.superblockly.blocks.voidBlocks.VariableDeclarationBlock
+/*
+fun main() {
+    var start = StartBlock()
+    var variableBlock = VariableDeclarationBlock("X")
+    connectTo(start.nextBlockConnector, variableBlock.topConnector)
+    connec
+}
+*/
