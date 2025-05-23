9db207d2a68200970a782339f3be376c98ed8a21
Kotlovskiy <88842353+Kotlovskiy@users.noreply.github.com>
2025-05-23 10:51:40 +0700

    Delete app/src/main/java/com/unewexp/superblockly/viewBlocks/ReturtViewBlock.kt
    устаревшая логика

--- app/src/main/java/com/unewexp/superblockly/viewBlocks/ReturtViewBlock.kt
+++
@@ -1,6 +0,0 @@
-package com.unewexp.superblockly.viewBlocks
-
-import androidx.compose.ui.unit.Dp
-
-abstract class ReturnViewBlock(initialX: Dp, initialY: Dp) : InlineViewBlock(initialX, initialY) {
-}
